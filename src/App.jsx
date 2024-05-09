a9973d907af170b3c08d9087a7e31dbb3c7bd965
integrader <integrader21@gmail.com>
2024-05-09 07:55:54 +0530

    File structure for first project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-red-400'> hii buddy this is my you know!</h1>
+  )
+}
+
+export default App
